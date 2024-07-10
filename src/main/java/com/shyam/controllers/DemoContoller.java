be9b2225cded9a132f1ca77a81540ee9b90da3a6
KarnamShyam1947 <karnamshyamgithub@gmail.com>
2024-07-10 18:43:21 +0530

    added: actuator support for montioring

--- src/main/java/com/shyam/controllers/DemoContoller.java
+++ src/main/java/com/shyam/controllers/DemoContoller.java
@@ -2,12 +2,16 @@ package com.shyam.controllers;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoContoller {
+
+    Logger logger = LoggerFactory.getLogger(DemoContoller.class);
 
     @Value("${spring.application.name}")
     private String appName;
@@ -17,6 +21,12 @@ public class DemoContoller {
         return Map.of("name", "karnam shyam",
                       "app-name", appName,
                       "email", "karnamshyam9009@gmail.com");
+    }
+    
+    @GetMapping("/")
+    public Object sampleTest() {
+        logger.warn("Testing warn logs....");
+        return Map.of("testing", "okk");
     }
 
 }
