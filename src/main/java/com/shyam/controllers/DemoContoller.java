110a0cd97829cf87bc7f88989ffb6a7f886b16a1
KarnamShyam1947 <karnamshyamgithub@gmail.com>
2024-07-10 17:14:26 +0530

    app setup done

---
+++ src/main/java/com/shyam/controllers/DemoContoller.java
@@ -0,0 +1,22 @@
+package com.shyam.controllers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoContoller {
+
+    @Value("${spring.application.name}")
+    private String appName;
+    
+    @GetMapping("/")
+    public Object sample() {
+        return Map.of("name", "karnam shyam",
+                      "app-name", appName,
+                      "email", "karnamshyam9009@gmail.com");
+    }
+
+}
